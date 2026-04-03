057309d387866065c137bc71e8f6a3dce32b4f60
Rodrigo Nochez <00378021@uca.edu.sv>
2026-04-03 15:14:31 -0700

    feat: first commit for CIS 279 Project 1

---
+++ linkedlist.h
@@ -0,0 +1,109 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+template <typename T>
+class LinkedList {
+private:
+    struct Node {
+        T data;
+        Node* next;
+        //Constructor
+        Node(T val) : data(val), next(nullptr) {}
+    };
+
+    Node* head;
+    int currentSize;
+
+public:
+    LinkedList() : head(nullptr), currentSize(0) {}
+
+    // Add a node to the front
+    void insertFront(T val);
+
+    // Removes the first occurrence of a value
+    bool remove(T val);
+
+    // Print all elements
+    void display() const;
+
+    // Returns the current size
+    int size() const { return currentSize; }
+
+    // Destructor
+    ~LinkedList();
+};
+
+template <typename T>
+void LinkedList<T>::insertFront(T val) {
+    
+    Node* newNode = new Node(val);
+
+    newNode->next = head;
+
+    head = newNode;
+
+    currentSize++;
+}
+
+template<typename T>
+bool LinkedList<T>::remove(T val) {
+
+    if (head == nullptr) return false;
+
+    if (head->data == val) {
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        currentSize--;
+        return true;
+    }
+
+    Node* prev = head;
+    Node* curr = head->next;
+
+    while(curr) {
+        if (curr->data == val) {
+            prev->next = curr->next;
+            delete curr;
+            currentSize--;
+            return true;
+        }
+
+        prev = curr;
+        curr = curr->next;
+    }
+
+    return false;
+}
+
+template<typename T>
+void LinkedList<T>::display() const {
+
+    Node* curr = head;
+
+    while(curr) {
+        std::cout << curr->data << ", ";
+        curr = curr->next;
+    }
+    std::cout << "nullptr" << std::endl;
+}
+
+template<typename T>
+LinkedList<T>::~LinkedList() {
+    
+    Node* curr = head;
+
+    while(curr) {
+        Node* nextNode = curr->next;
+
+        delete curr;
+
+        curr = nextNode;
+    }
+
+    head = nullptr;
+    currentSize = 0;
+
+}
+
+#endif
